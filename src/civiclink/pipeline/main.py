bb923a27ba37182e3311fa8816211e44d34889f5
Duong Tran <d.tran.1@campus.tu-berlin.de>
2025-11-15 12:40:10 +0100

    refactor: refactor into mul funcs

--- src/civiclink/pipeline/main.py
+++ src/civiclink/pipeline/main.py
@@ -10,28 +10,30 @@ from sklearn.metrics import roc_auc_score
 
 from civiclink.utils import get_root_dir
 
-def main(test_samples):
-    # load data
+def load_data_train_test():
     root_dir = get_root_dir()
     fpath = root_dir/"data/fake_steuerdaten_labels_not_random.csv"
     df = pd.read_csv(fpath, delimiter=";")
-
     X,y = df.drop(columns="Label"), df["Label"]
 
     for col in ["Familienstand", "Steuerklasse", "Bundesland","Religionszugehörigkeit", 
                 "Einkunftsart","Branche_Selbstaendig"]:
         if col in X.columns:
             X[col] = X[col].astype("category")
-
-    #X["Summe_Einkuenfte_Brutto"].astype("float")
-
+            
     X_train, X_test, y_train, y_test = train_test_split(X, y, test_size= 0.2, random_state= 1, shuffle = True)
 
+    return X_train, X_test, y_train, y_test
+
+
+def train(X_train, y_train):
     bst = XGBClassifier(enable_categorical=True)
-
     bst.fit(X_train, y_train)
-    explainer = shap.TreeExplainer(bst)
+    
+    return bst
 
+def get_shap_fig(test_samples, bst):
+    explainer = shap.TreeExplainer(bst)
     X_pos = test_samples
     X_sample = X_pos.iloc[0:1]
     shap_values  = explainer.shap_values(X_sample)
